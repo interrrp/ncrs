32fc3fd2a94ff7eaed679ced93896a147fcfefe7
interrrp <int@addictedto.coffee>
2023-10-28 10:42:55 +0800

    Client implementation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
+use std::{io, net::TcpStream, sync::Arc, thread};
+
 use clap::Parser;
 
 use ncrs::Args;
 
-fn main() {
+fn main() -> io::Result<()> {
     let args = Args::parse();
-    println!("Connecting to {}", args.address);
+
+    let stream = match TcpStream::connect(&args.address) {
+        Ok(stream) => stream,
+        Err(e) => {
+            eprintln!("Could not connect to {}: {}", args.address, e);
+            return Ok(());
+        }
+    };
+
+    // Since `io::copy` is blocking, we use a separate thread to copy from stdin to the stream and
+    // use the main thread to copy from the stream to stdout.
+    let stream = Arc::new(stream);
+    let stream_clone = Arc::clone(&stream);
+    thread::spawn(move || -> io::Result<()> {
+        io::copy(&mut io::stdin(), &mut &*stream_clone)?;
+        Ok(())
+    });
+    io::copy(&mut &*stream, &mut io::stdout())?;
+
+    Ok(())
 }
